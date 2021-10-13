bfbf78a42d07e47faf9b8cf89c75530735ff354b
ayeshaazeema <ayesha.azeema@gmail.com>
2021-10-13 14:51:08 +0700

    Second commit

--- app/src/main/java/com/ayeshaazeema/dzikirapp/adapter/ArtikelAdapter.kt
+++ app/src/main/java/com/ayeshaazeema/dzikirapp/adapter/ArtikelAdapter.kt
@@ -1,4 +1,41 @@
 package com.ayeshaazeema.dzikirapp.adapter
 
-class ArtikelAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.ayeshaazeema.dzikirapp.databinding.ItemArtikelBinding
+import com.ayeshaazeema.dzikirapp.model.Artikel
+
+class ArtikelAdapter(private val listArtikel: ArrayList<Artikel>) :
+    RecyclerView.Adapter<ArtikelAdapter.MyViewHolder>() {
+
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    inner class MyViewHolder(var itemArtikelBinding: ItemArtikelBinding) :
+        RecyclerView.ViewHolder(itemArtikelBinding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtikelAdapter.MyViewHolder {
+        val itemArtikelBinding =
+            ItemArtikelBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(itemArtikelBinding)
+    }
+
+    override fun onBindViewHolder(holder: ArtikelAdapter.MyViewHolder, position: Int) {
+        holder.itemArtikelBinding.ivArtikel.setImageResource(listArtikel[position].imageArtikel)
+        holder.itemView.setOnClickListener {
+            onItemClickCallback.onItemClicked(listArtikel[position])
+        }
+    }
+
+    override fun getItemCount(): Int = listArtikel.size
+}
+
+interface OnItemClickCallback {
+    fun onItemClicked(data: Artikel)
 }
